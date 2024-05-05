b3446049c5b42c2795749fc8a65abc9fa77499d3
Atmaram Korgaonkar <120163458+Viraj0510@users.noreply.github.com>
2024-05-05 12:10:24 +0530

    Update ascending-descending.c

--- ascending-descending.c
+++ ascending-descending.c
@@ -5,7 +5,7 @@
 
 void main(void){
     int i,j,temp=0x00;
-   int a={0x03,0x05,0x02,0x0a};
+   int a[]={0x03,0x05,0x02,0x0a};
    for(i=0;i<4;i++){
        for(j=0;j<4-i;j++){
            if(a[j]>a[j+1]){
